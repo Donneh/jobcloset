c538097c266d60dcc0aa3e3fd61220e5efe23faa
Donny van Walsem <donnehvw@gmail.com>
2023-06-14 00:00:15 +0200

    Add money cast

--- database/migrations/2023_05_31_230631_create_products_table.php
+++ database/migrations/2023_05_31_230631_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->decimal('price', 10, 2);
+            $table->string('currency', 3)->default('EUR');
             $table->integer('stock');
             $table->text('description')->nullable();
             $table->string('image_path')->nullable();
