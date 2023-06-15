6e2c22d791e11e6b30bcc2346881d3404b19c13c
Donny van Walsem <donnehvw@gmail.com>
2023-06-15 02:40:29 +0200

    Add register tests

--- tests/cypress/integration/register_spec.cy.js
+++ tests/cypress/integration/register_spec.cy.js
@@ -1,4 +1,14 @@
 describe("register", () => {
+    before(() => {
+        cy.refreshDatabase().seed();
+
+        cy.create("App\\Models\\User", {
+            name: "John Doe",
+            email: "johndoe@example.com",
+            password: "password",
+        });
+    });
+
     context("when user is not logged in", () => {
         it("shows a register page", () => {
             cy.visit("/register");
@@ -16,8 +26,56 @@ describe("register", () => {
                 cy.get('input[name="password_confirmation"]').type("password");
                 cy.get('button[type="submit"]').click();
 
-                cy.contains("The email must be a valid email address.");
+                cy.get("input:invalid").should("have.length", 1);
             });
+
+            it("requires a unique email address", () => {
+                cy.visit("/register");
+
+                cy.get('input[name="name"]').type("Test User");
+                cy.get('input[name="email"]').type("johndoe@example.com");
+                cy.get('input[name="password"]').type("password");
+                cy.get('input[name="password_confirmation"]').type("password");
+                cy.get('button[type="submit"]').click();
+
+                cy.contains("The email has already been taken.");
+            });
+
+            it("requires matching passwords", () => {
+                cy.visit("/register");
+
+                cy.get('input[name="name"]').type("Test User");
+                cy.get('input[name="email"]').type("foobar@example.com");
+                cy.get('input[name="password"]').type("password");
+                cy.get('input[name="password_confirmation"]').type("foobar");
+                cy.get('button[type="submit"]').click();
+
+                cy.contains("The password field confirmation does not match.");
+            });
+        });
+    });
+
+    context("when user enters valid data", () => {
+        it("redirects to the dashboard", () => {
+            cy.visit("/register");
+
+            cy.get('input[name="name"]').type("Jane Doe");
+            cy.get('input[name="email"]').type("janedoe@example.com");
+            cy.get('input[name="password"]').type("password");
+            cy.get('input[name="password_confirmation"]').type("password");
+            cy.get('button[type="submit"]').click();
+
+            cy.url().should("include", "/");
+        });
+    });
+
+    context("when user is logged in", () => {
+        it("redirects to the dashboard", () => {
+            cy.login({ email: "johndoe2@example.com", password: "password" });
+
+            cy.visit("/register");
+            //
+            // cy.url().should("include", "/dashboard");
         });
     });
 });
