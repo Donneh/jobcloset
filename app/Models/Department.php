c5a238b8e29e75022755f46b22057f966340cdc7
Donny van Walsem <donnehvw@gmail.com>
2023-09-12 23:24:06 +0200

    Add department_user migration

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToTenant;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
