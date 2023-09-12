1595b585d0c196cf42ec4fb201573584c4e29672
Donny van Walsem <donnehvw@gmail.com>
2023-09-12 22:49:48 +0200

    Add test for creating departments

---
+++ tests/Feature/Departments/DepartmentTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\User;
+
+it('can add department', function () {
+    $this->user = User::factory()->create();
+    $department = Department::factory()->create();
+
+    $response = $this->actingAs($this->user)->get('/departments');
+
+    $response
+        ->assertStatus(200)
+        ->assertSee($department->name);
+});
