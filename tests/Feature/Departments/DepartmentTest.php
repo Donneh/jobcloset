4597b09f7aba4582f8f8ec2261d058b16c24c32b
Donny van Walsem <donnehvw@gmail.com>
2023-09-14 01:11:14 +0200

    Add more tests to DepartmentTest

--- tests/Feature/Departments/DepartmentTest.php
+++ tests/Feature/Departments/DepartmentTest.php
@@ -2,6 +2,48 @@
 
 use App\Models\Department;
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
+
+it('shows all departments', function() {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Department::factory()->create(['name' => 'Sales']);
+    Department::factory()->create(['name' => 'Marketing']);
+    Department::factory()->create(['name' => 'Engineering']);
+
+    get(route('department.index'))
+        ->assertSee('Sales')
+        ->assertSee('Marketing')
+        ->assertSee('Engineering');
+});
+
+it('can show a department', function() {
+
+});
+
+it('can update a department', function() {
+
+});
+
+it('can delete a department', function() {
+
+});
+
+it('only shows departments for the current tenant', function() {
+    $otherTenantsDepartments = Department::factory(5)->create();
+    $user = User::factory()->create();
+    $tenantDepartments = Department::factory(5)->create(['tenant_id' => $user->tenant_id]);
+
+    $foo = Department::all();
+
+    $this->actingAs($user)->get(route('department.index'))
+        ->assertSee($tenantDepartments->first()->name)
+        ->assertDontSee($otherTenantsDepartments->first()->name);
+});
 
 it('can create a department', function () {
     $this->user = User::factory()->create();
@@ -14,3 +56,4 @@ it('can create a department', function () {
         'tenant_id' => $department->tenant_id,
     ]);
 });
+
