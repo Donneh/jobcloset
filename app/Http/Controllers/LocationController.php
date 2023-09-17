6ff4b7baff248bd794abdae896ac42b0d0753801
Donny van Walsem <donnehvw@gmail.com>
2023-09-17 04:46:06 +0200

    WIP

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocationController extends Controller{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Location;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class LocationFactory extends Factory{
+    protected $model = Location::class;
+
+    public function definition(): array
+    {
+        return [
+            'created_at' => Carbon::now(),//
+'updated_at' => Carbon::now(),
+        ];
+    }
+}
