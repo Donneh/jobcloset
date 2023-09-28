901ffbb5d901b7ec40fb1de76884aca96961cf95
Donny van Walsem <donnehvw@gmail.com>
2023-09-28 02:40:53 +0200

    Move webhook to own routes file

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'webhook/*',
-        'payment/webhook/*'
+        'payment/webhook/*',
+        'payment/webhook',
     ];
 }
