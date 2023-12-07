2aac9a5e94fac06ecb098ab1a152df9925bd68b6
Donny van Walsem <donnehvw@gmail.com>
2023-12-07 02:49:45 +0100

    Add Docker configuration and update package dependencies
    This commit introduces a Docker compose configuration for the Laravel test environment. It also updates various package dependencies across composer.json and composer.lock files, ensuring compatibility and up-to-date functionality. Additionally, minor tweaks are made to the Vite configuration for localhost.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
