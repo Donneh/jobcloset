0bd2817441aa1f7577f78c392b3a047dfc628014
Donny van Walsem <donnehvw@gmail.com>
2023-12-11 20:25:49 +0100

    Add department relationship and filtering to products
    A many-to-many relationship between Products and Departments has been added, along with a new intermediate Laravel migration. Both models have been updated with necessary relationship methods. In ListProducts, Select components were added for the department_ids attribute. Additionally, the products displayed in ShopList are filtered based on the user's departments.

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -14,10 +14,16 @@ class Department extends Model
 
     protected $fillable = [
         'name',
+        'tenant_id'
     ];
 
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
     }
 }
